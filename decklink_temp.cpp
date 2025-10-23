abc9acbf6962be839afa7176049a49e973b56ce6
Justin Osborne <justin@eblah.com>
2025-10-23 11:38:50 -0500

    Add device management functions and update version to 1.1.0

--- decklink_temp.cpp
+++ decklink_temp.cpp
@@ -1,6 +1,91 @@
 #include <napi.h>
 #include "DeckLinkAPI.h"
 
+Napi::Value GetAvailableDevices(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    IDeckLinkIterator* deckLinkIterator = CreateDeckLinkIteratorInstance();
+    if (!deckLinkIterator) {
+        Napi::TypeError::New(env, "Unable to create DeckLink iterator").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    IDeckLink* deckLink = nullptr;
+    int deviceCount = 0;
+
+    while (deckLinkIterator->Next(&deckLink) == S_OK) {
+        if (deckLink) {
+            deviceCount++;
+            deckLink->Release();
+        }
+    }
+
+    deckLinkIterator->Release();
+    return Napi::Number::New(env, deviceCount);
+}
+
+Napi::Value GetDeviceOutputs(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    if (info.Length() < 1 || !info[0].IsNumber()) {
+        Napi::TypeError::New(env, "Device index required").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    int deviceIndex = info[0].As<Napi::Number>().Int32Value();
+    if (deviceIndex < 0) {
+        Napi::TypeError::New(env, "Device index must be >= 0").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    IDeckLinkIterator* deckLinkIterator = CreateDeckLinkIteratorInstance();
+    if (!deckLinkIterator) {
+        Napi::TypeError::New(env, "Unable to create DeckLink iterator").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    IDeckLink* deckLink = nullptr;
+    HRESULT result = S_OK;
+    int currentIndex = 0;
+    while (currentIndex <= deviceIndex) {
+        result = deckLinkIterator->Next(&deckLink);
+        if (result != S_OK || !deckLink) {
+            deckLinkIterator->Release();
+            Napi::TypeError::New(env, "Device index out of range").ThrowAsJavaScriptException();
+            return env.Null();
+        }
+        if (currentIndex < deviceIndex) {
+            deckLink->Release();
+            deckLink = nullptr;
+        }
+        currentIndex++;
+    }
+    deckLinkIterator->Release();
+
+    IDeckLinkAttributes* deckLinkAttributes = nullptr;
+    int64_t outputConnections = 0;
+
+    if (deckLink->QueryInterface(IID_IDeckLinkAttributes, (void**)&deckLinkAttributes) == S_OK) {
+        if (deckLinkAttributes->GetInt(BMDDeckLinkVideoOutputConnections, &outputConnections) == S_OK) {
+            deckLinkAttributes->Release();
+            deckLink->Release();
+
+            // Count the number of output connections by checking bits
+            int outputCount = 0;
+            for (int i = 0; i < 32; i++) {
+                if (outputConnections & (1LL << i)) {
+                    outputCount++;
+                }
+            }
+
+            return Napi::Number::New(env, outputCount);
+        }
+        deckLinkAttributes->Release();
+    }
+
+    deckLink->Release();
+    Napi::TypeError::New(env, "Unable to get device output count").ThrowAsJavaScriptException();
+    return env.Null();
+}
+
 Napi::Value GetDeviceTemperature(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     if (info.Length() < 1 || !info[0].IsNumber()) {
@@ -54,6 +139,8 @@ Napi::Value GetDeviceTemperature(const Napi::CallbackInfo& info) {
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set(Napi::String::New(env, "getDeviceTemperature"), Napi::Function::New(env, GetDeviceTemperature));
+    exports.Set(Napi::String::New(env, "getAvailableDevices"), Napi::Function::New(env, GetAvailableDevices));
+    exports.Set(Napi::String::New(env, "getDeviceOutputs"), Napi::Function::New(env, GetDeviceOutputs));
     return exports;
 }
 
--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,3 @@
 export function getDeviceTemperature(deviceIndex: number): number;
+export function getAvailableDevices(): number;
+export function getDeviceOutputs(deviceIndex: number): number;
